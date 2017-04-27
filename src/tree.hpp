f837f9d4f41756c0ab1cd8fe7b161faad9eaf47c
MaanooAk <akritasak@gmail.com>
2017-04-27 18:33:25 +0300

    Abstract tree class

---
+++ src/tree.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <ostream>
+
+
+template <typename T>
+class Optional {
+public:
+	
+	Optional() : has_value(false) { }
+	Optional(const T & value) : has_value(true), value(value) { }
+
+	bool has() { return has_value; }
+	T & get() { return value; }
+
+protected:
+	
+	bool has_value;
+	T value;
+	
+private:	
+};
+	
+template <typename T>
+class AbstractTree {
+public:
+	
+	virtual Optional<T> find(const T & element) const = 0;
+	
+	virtual void insert(const T & element) = 0;
+	
+	virtual void remove(const T & element) = 0;
+	
+	virtual void clear() {
+		throw "Not implemented";
+	}
+	
+	virtual bool empty() const = 0;
+	
+	virtual void print(std::ostream & stream) const {
+		throw "Not implemented";
+	}
+	
+protected:
+	
+private:
+	
+};
