544e0bf6723372a99ad4f11c4139b5ce44a9037e
MaanooAk <akritasak@gmail.com>
2017-05-05 10:52:38 +0300

    Changed find header

--- src/tree.hpp
+++ src/tree.hpp
@@ -6,7 +6,7 @@
 template <typename T>
 class Optional {
 public:
-	
+
 	Optional() : has_value(false) { }
 	Optional(const T & value) : has_value(true), value(value) { }
 
@@ -14,35 +14,35 @@ public:
 	T & get() { return value; }
 
 protected:
-	
+
 	bool has_value;
 	T value;
-	
-private:	
+
+private:
 };
-	
+
 template <typename T>
 class AbstractTree {
 public:
-	
-	virtual Optional<T> find(const T & element) const = 0;
-	
+
+	virtual Optional<T> find(const T & element) = 0;
+
 	virtual void insert(const T & element) = 0;
-	
+
 	virtual void remove(const T & element) = 0;
-	
+
 	virtual void clear() {
 		throw "Not implemented";
 	}
-	
+
 	virtual bool empty() const = 0;
-	
+
 	virtual void print(std::ostream & stream) const {
 		throw "Not implemented";
 	}
-	
+
 protected:
-	
+
 private:
-	
+
 };
